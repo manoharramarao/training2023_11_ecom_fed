7e4823d026931a6bf074a8745fcf9639e4582728
Manohar Ramarao <manoharramarao@gmail.com>
2023-12-07 20:06:42 +0530

    added state to register component

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,9 +1,32 @@
+'use client'
+
 import { Box, Button, Checkbox, Container, Grid, TextField, Typography } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 
 const RegisterPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [errPass, setErrPass] = React.useState("");
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  useEffect(() => {
+    if (password === null || password === undefined || password === "") {
+      setErrPass("Password is required");
+    } else {
+      setErrPass("");
+    }
+  },[password])
+
+  function register() {
+    console.log(`Need to call backend API to register with values ${email} and ${password}`);
+  }
 
   return (
     <>
@@ -16,18 +39,19 @@ const RegisterPage = () => {
               </Grid>
               <Box pb={4}></Box>
               <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <TextField fullWidth variant="outlined" label="Email" type="email"></TextField>
+                <TextField fullWidth variant="outlined" label="Email" type="email" onChange={handleEmailChange}></TextField>
               </Grid>
               <Box pb={1}></Box>
               <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <TextField fullWidth variant="outlined" label="Password" type="password"></TextField>
+                <TextField fullWidth variant="outlined" label="Password" type="password" onChange={handlePasswordChange}
+                helperText={errPass}></TextField>
               </Grid>
               <Box pb={2}></Box>
               <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
                 <Checkbox />I agree to the terms and conditions!
               </Grid>
               <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                <Button variant="contained" disableElevation fullWidth size="large">
+                <Button variant="contained" disableElevation fullWidth size="large" onClick={register}>
                   Sign up
                 </Button>
               </Grid>
